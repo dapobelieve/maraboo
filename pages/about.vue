94eeeaa6de2efe2a469d5b6ba440d9c2995f3c43
dapo-maraboo <oladapo@mara.boo>
2023-05-06 12:54:53 +0100

    fix: about us

--- pages/about.vue
+++ pages/about.vue
@@ -2,23 +2,20 @@
   <main class="container mx-auto px-2">
     <section class="py-12 md:pb-32 pt-24">
       <div class="flex flex-col items-center">
-        <h1 class="text-purple text-3xl mb-8 font-extrabold">About Maraboo</h1>
+        <h1 class="text-purple text-3xl mb-8 font-extrabold">
+          {{ $t("about-us.name") }}
+        </h1>
         <h1
           class="font-heading text-4xl md:text-6xl mb-8 max-w-[65rem] text-center leading-snug"
         >
-          We're building borderless financial systems to connect West-Africans
-          across the globe
+          {{ $t("about-us.caption") }}
         </h1>
         <p class="text-center text-lg max-w-[39rem] leading-normal">
-          <!--          <span v-html="$t('about-us.text')"> </span>-->
-          <span class="text-purple">Introducing Maraboo</span>, a West-African
-          fintech firm offering transformative financial solutions that are
-          fair, responsible, inclusive, and designed to improve the quality of
-          life of West Africans.
+          <span v-html="$t('about-us.text')"> </span>
         </p>
       </div>
       <div class="mt-16 text-center">
-        <GlobalMore> Read More </GlobalMore>
+        <GlobalMore> {{ $t("about-us.read-more") }} </GlobalMore>
       </div>
     </section>
     <section class="countries py-12 md:py-32">
@@ -27,15 +24,10 @@
           <div class="flex text-left flex-col px-4 h-full">
             <h1
               class="font-heading text-4xl max-w-[45rem] md:text-6xl leading-none mb-4 md:mb-8"
-            >
-              Our Core mission is to make payments in & out of WAEMU countries
-              up <span class="text-purple">to 3x cheaper!</span>
-              <br class="hidden md:block" />
-            </h1>
+              v-html="$t('about-us.section-2.caption')"
+            ></h1>
             <div class="md text-xl md:w-[30rem] text-left mb-8 leading-relaxed">
-              Did you know that a 10% reduction in transaction fees for sending
-              money in & out of WAEMU country can boost the revenue of regional
-              households by up to 16%?
+              {{ $t("about-us.section-2.text") }}
             </div>
           </div>
         </div>
@@ -64,17 +56,12 @@
             <h1
               class="font-heading md:text-left text-4xl max-w-[45rem] md:text-6xl leading-none mb-8 md:mb-16"
             >
-              We believe that remittances should be fast, low cost, and have a
-              major social impact.
+              {{ $t("about-us.section-2.we-believe") }}
             </h1>
             <p
               class="md text-xl md:mb-8 text-justify leading-relaxed max-w-[34rem]"
-            >
-              Two birds one stone - we apply the real exchange rate on all of
-              our transactions to save you money and make the
-              <span class="text-purple"> UN social development goal </span> by
-              significantly reducing remittance cost by 2030 a reality today!
-            </p>
+              v-html="$t('about-us.section-2.two')"
+            ></p>
           </div>
         </div>
         <div
